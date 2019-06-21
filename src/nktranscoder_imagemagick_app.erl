9e00dfeed1c81a41ac794c57503faccc5dfc0113
Carlos Gonzalez <carlosj.gf@gmail.com>
2019-06-21 10:56:46 +0200

    First commit

---
+++ src/nktranscoder_imagemagick_app.erl
@@ -0,0 +1,84 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 NetScale, SL.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(nktranscoder_imagemagick_app).
+-author('Carlos Gonzalez <carlosj.gf@gmail.com>').
+-behaviour(application).
+
+-export([start/0, start/2, stop/1]).
+-export([get/1, get/2, put/2, del/1]).
+
+-include("nktranscoder_imagemagick.hrl").
+
+-define(APP, nktranscoder_imagemagick).
+-compile({no_auto_import, [get/1, put/2]}).
+
+%% ===================================================================
+%% Private
+%% ===================================================================
+
+%% @doc Starts NkSERVER stand alone.
+-spec start() ->
+    list().
+
+start() ->
+    application:ensure_all_started(?APP).
+
+
+%% @doc
+start(_Type, _Args) ->
+    Syntax = #{
+    },
+    case nklib_config:load_env(?APP, Syntax) of
+        {ok, _} ->
+            {ok, Pid} = nktranscoder_imagemagick_sup:start_link(),
+            {ok, Vsn} = application:get_key(nktranscoder_imagemagick, vsn),
+            lager:info("NkSERVER TRANSCODER ImageMagick v~s has started.", [Vsn]),
+            {ok, Pid};
+        {error, Error} ->
+            lager:error("Error parsing config: ~p", [Error]),
+            error(Error)
+    end.
+
+
+
+%% @private OTP standard stop callback
+stop(_) ->
+    ok.
+
+
+%% @doc gets a configuration value
+get(Key) ->
+    get(Key, undefined).
+
+
+%% @doc gets a configuration value
+get(Key, Default) ->
+    nklib_config:get(?APP, Key, Default).
+
+
+%% @doc updates a configuration value
+put(Key, Value) ->
+    nklib_config:put(?APP, Key, Value).
+
+
+%% @doc updates a configuration value
+del(Key) ->
+    nklib_config:del(?APP, Key).
---
+++ src/nktranscoder_imagemagick_sup.erl
@@ -0,0 +1,36 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 NetScale, SL.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+-module(nktranscoder_imagemagick_sup).
+-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+
+%% @private
+start_link() ->
+    ChildsSpec = [
+    ],
+    supervisor:start_link({local, ?MODULE}, ?MODULE,
+        {{one_for_one, 10, 60}, ChildsSpec}).
+
+%% @private
+init(ChildSpecs) ->
+    {ok, ChildSpecs}.
+
+
